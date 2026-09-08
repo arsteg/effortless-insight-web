8c8bb9e16953892d1838204c9aab442b7c6bda39
Sandeep Kumar <sandeepkumar.chipsoft@gmail.com>
2026-09-08 12:19:01 +0530

    Aws build issue
    Aws build issue

--- src/app/delete-account/page.tsx
+++ src/app/delete-account/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
 import Image from 'next/image'
+import { COMPANY } from '@/lib/company'
 
 export const metadata: Metadata = {
   title: 'Account Deletion | EffortlessInsight',
